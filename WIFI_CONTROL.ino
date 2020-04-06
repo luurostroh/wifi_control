7d098de454d387314ed403e77558bfb03186c75b
Ludek <ludek.urbanic@partec.cz>
2020-04-06 12:51:22 +0200

    test 2

--- WIFI_CONTROL.ino
+++ WIFI_CONTROL.ino
@@ -1,7 +1,7 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266WiFi.h>
 #include <WebSocketsServer.h>
-// test
+// test 2
 ESP8266WebServer server;
 WebSocketsServer webSocket = WebSocketsServer(81);
 typedef struct {
